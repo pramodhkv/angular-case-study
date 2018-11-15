5b4fee23dcee0a83f9623c5d2b5974e817c78660
Viswanath <VISWAP@de.softlab.net>
2018-11-15 13:22:24 +0100

    Added bootstrap and header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
+import { WorkAreaComponent } from './work-area/work-area.component';
+import { WorkAreaOverviewComponent } from './work-area-overview/work-area-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    WorkAreaComponent,
+    WorkAreaOverviewComponent
   ],
   imports: [
     BrowserModule,
