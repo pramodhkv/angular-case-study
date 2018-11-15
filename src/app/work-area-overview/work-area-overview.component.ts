334326b7f5e620cfa56e804f5ef60d722f16e252
Viswanath <VISWAP@de.softlab.net>
2018-11-15 13:21:46 +0100

    Added 3 components

---
+++ src/app/work-area-overview/work-area-overview.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-work-area-overview',
+  templateUrl: './work-area-overview.component.html',
+  styleUrls: ['./work-area-overview.component.css']
+})
+export class WorkAreaOverviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
