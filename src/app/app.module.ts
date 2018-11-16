4dd3ac24e93ac266ac004c6167b5e928ac9b5a45
Viswanath <VISWAP@de.softlab.net>
2018-11-16 09:48:39 +0100

    Changed menu component and work area component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,50 @@ import { WorkAreaComponent } from './work-area/work-area.component';
 import { WorkAreaOverviewComponent } from './work-area-overview/work-area-overview.component';
 import { MenuItemComponent } from './menu/menu-item/menu-item.component';
 
+import { FormsModule } from "@angular/forms";
+
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {DatepickerModule, BsDatepickerModule} from 'ngx-bootstrap/datepicker';
+
+
+import { 
+  MatAutocompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatFormFieldModule } from '@angular/material';
+import { DropdownDirective } from './shared/dropdown.directive';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,11 +60,49 @@ import { MenuItemComponent } from './menu/menu-item/menu-item.component';
     MenuComponent,
     WorkAreaComponent,
     WorkAreaOverviewComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    DropdownDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    DatepickerModule.forRoot(),
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/work-area/work-area.component.ts
+++ src/app/work-area/work-area.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormControl} from '@angular/forms';
+import { Activity } from './activity.model';
+
 @Component({
   selector: 'app-work-area',
   templateUrl: './work-area.component.html',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkAreaComponent implements OnInit {
 
+  date = new FormControl(new Date());
+
+  activities: Activity[] = [
+    new Activity("Development", 1),
+    new Activity("Client Meeting", 2),
+    new Activity("Demo", 3),
+    new Activity("Testing", 4),
+    new Activity("Documentation", 5),
+    new Activity("Code Review", 6),
+    new Activity("Session", 7),
+    new Activity("Deployment", 8),
+    new Activity("Management Activity", 9),
+    new Activity("Production Support", 10),
+  ];
+
+  projects: Activity[] = [
+    new Activity("Spectra Labs", 1),
+    new Activity("Symfonie", 2),
+    new Activity("BMW EFA", 3),
+    new Activity("BMW CASA", 4),
+    new Activity("BMW ISPA Trac Zentral", 5),
+    new Activity("BMW IVS-R", 6)
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
